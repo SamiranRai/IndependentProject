e5e9cc6a417854ef17a9a98fc2b9f1fa711244d3
Samiran Ray <SamiranRai@users.noreply.github.com>
2026-02-02 23:58:36 +0530

    fix: webhook can safely recive and pass to webhook service layer

--- src/controllers/inbound.controller.js
+++ src/controllers/inbound.controller.js
@@ -1,42 +1,45 @@
-const AppError = require("../middlewares/AppError");
 const catchAsync = require("../middlewares/catchAsync");
 const WebhookSourceModel = require("../models/webhookSource.model");
 const inboundService = require("../services/inbound.service");
+const verifyFramerSignature = require("../utils/verifyFramerSignature");
 const verifyWebflowSignature = require("../utils/verifyWebflowSignature");
 
-// CONTROLLER: INBOUND EMAIL CONTROLLER
-exports.inboundEmail = catchAsync(async (req, res, next) => {
-  // MAILGUN SEND THE PAYLOAD
-  const payload = req.body;
-
-  await inboundService.processInboundEmailService(payload);
-  res.status(200).send("OK");
-});
-
-//webhookUrl: /api/v1/inbound/webhook/${identifier}, --type=webflow,framer,custom
 // CONTROLLER: INBOUND WEBHOOK CONTROLLER
-exports.inboundWebhook = catchAsync(async (req, res, next) => {
-  const { identifier } = req.params;
+exports.inboundWebhookController = catchAsync(async (req, res, next) => {
+  const { webhookIdentifier } = req.params;
 
-  // FIND THE WEBHOOK OWNER
-  const source = await WebhookSourceModel.findOne({
-    identifier,
-    status: "ACTIVE",
+  const webhookSource = await WebhookSourceModel.findOne({
+    identifier: webhookIdentifier,
+    status: "active",
     deletedAt: null,
   }).select("+secret");
 
-  if (!source) {
+  if (!webhookSource) {
     return res.status(404).end();
   }
 
-  // EXPLICIT CHECK FOR WEBFLOW
-  if (source.provider === "webflow") {
-    // VALIDATE THE WEBFLOW REQ
-    verifyWebflowSignature(req, source.secret);
+  const signatureVerifierFunction = {
+    webflow: verifyWebflowSignature,
+    framer: verifyFramerSignature,
+  };
+  const webhookSignatureVerifier =
+    signatureVerifierFunction[webhookSource.provider];
+  const isWebhookSignatureValid = webhookSignatureVerifier(
+    req,
+    webhookSource.secret
+  );
+  if (!isWebhookSignatureValid) {
+    console.warn("Invalid webhook signature", {
+      provider: webhookSource.provider,
+      identifier: webhookSource.identifier,
+    });
+
+    return res.status(200).json({ ok: true });
   }
 
   res.status(200).json({ ok: true });
 
-  // CALL AFTER TO PREVENT FROM WEBFLOW REQ. AGAIN
-  await inboundService.processInboundWebhookService(req.body, source);
+  inboundService
+    .processInboundWebhookService(req.body, webhookSource)
+    .catch((err) => console.error("Inbound webhook processing failed", err));
 });
